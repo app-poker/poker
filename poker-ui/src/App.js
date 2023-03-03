4fa13182350ff75cf8b9eb0c3d9fb5c54756da32
brahmanand1 <44412993+brahmanand1@users.noreply.github.com>
2023-03-03 13:49:57 +0530

    removed unnecessery files

--- poker-ui/src/App.js
+++ poker-ui/src/App.js
@@ -1,6 +1,3 @@
-import "./App.css";
-import Button from "./components/button/Button";
-import im1 from "./image/landing.jpg";
 import Landing from "./components/pages/landing/Landing";
 
 function App() {
