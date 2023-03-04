728bb3f6898fc748117f828165d7a9e6700fe1ab
brahmanand1 <44412993+brahmanand1@users.noreply.github.com>
2023-03-04 23:37:32 +0530

    routing configured

--- poker-ui/src/App.js
+++ poker-ui/src/App.js
@@ -1,10 +1,16 @@
 import Landing from "./components/pages/landing/Landing";
+import { Route, Routes } from "react-router-dom";
+import Join from "./components/pages/join/Join";
+import Start from "./components/pages/start/Start";
 
 function App() {
- 
   return (
     <>
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />}/>
+        <Route path="/join" element={<Join />}/>
+        <Route path="/start" element={<Start />}/>
+      </Routes>
     </>
   );
 }
---
+++ poker-ui/src/components/pages/start/Start.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Start() {
+  return (
+    <div>This is start component</div>
+  )
+}
